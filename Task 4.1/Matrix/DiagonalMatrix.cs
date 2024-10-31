c802ea83b3b16c4da9afb2eb47663f854a06416f
Pablo <pablogp102@gmail.com>
2024-10-31 15:57:06 +0100

    Task 4.1 ready for rewiew

---
+++ Task 4.1/Matrix/DiagonalMatrix.cs
@@ -0,0 +1,73 @@
+﻿namespace Task_4._1.Matrix
+{
+    public class DiagonalMatrix<T>
+    {
+        private readonly T[] diagonalElements;
+        public int Size { get; }
+        public event EventHandler<ElementChangedEventArgs<T>> ElementChanged;
+
+        public DiagonalMatrix(int size)
+        {
+            if (size < 0) throw new ArgumentException("Size must be non-negative value.");
+            Size = size;
+            diagonalElements = new T[size];
+        }
+        public T this[int i, int j]
+        {
+            get 
+            {
+                ValidateIndices(i,j);
+                return i == j ? diagonalElements[i] : default(T);
+            }
+            set 
+            {
+                ValidateIndices(i, j);
+                if (i == j && !EqualityComparer<T>.Default.Equals(diagonalElements[i], value))
+                {
+                    var oldValue = diagonalElements[i];
+                    diagonalElements[i] = value;
+                    ElementChanged?.Invoke(this, new ElementChangedEventArgs<T>(i,j,oldValue, value));
+                }
+            }
+        }
+
+        private void ValidateIndices(int i, int j)
+        {
+            if(i < 0  || i >= Size || j < 0 || j >= Size)
+            {
+                throw new IndexOutOfRangeException("Indices are out of range");
+            }
+        }
+
+        public void DisplayMatrix()
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    if (i == j)
+                        Console.Write($"{diagonalElements[i]}\t");
+                    else
+                        Console.Write($"{default(T)}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    public class ElementChangedEventArgs<T> : EventArgs
+    {
+        public int Row { get; }
+        public int Column { get; }
+        public T OldValue {  get; }
+        public T NewValue {  get; }
+
+        public ElementChangedEventArgs(int row, int colum, T oldValue, T newValue)
+        {
+            Row = row;
+            Column = colum;
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
+    }
+}
---
+++ Task 4.1/Matrix/MatrixTracker.cs
@@ -0,0 +1,26 @@
+﻿namespace Task_4._1.Matrix
+{
+    public class MatrixTracker<T>
+    {
+        private readonly DiagonalMatrix<T> _matrix;
+        private readonly Stack<ElementChangedEventArgs<T>> _history = new();
+        public MatrixTracker(DiagonalMatrix<T> matrix)
+        {
+            _matrix = matrix ?? throw new ArgumentNullException(nameof(matrix));
+            _matrix.ElementChanged += OnElementChanged;
+        }
+        private void OnElementChanged(object sender, ElementChangedEventArgs<T> e)
+        { 
+            _history.Push(e);
+        }
+
+        public void Undo() 
+        { 
+            if(_history.Count > 0)
+            {
+                var lastChange = _history.Pop();
+                _matrix[lastChange.Row, lastChange.Column] = lastChange.OldValue;
+            }
+        }
+    }
+}
